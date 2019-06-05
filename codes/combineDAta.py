04649338e1f323f164185a6016d678512d39f813
claudiopacha <claudiomonteiro@protonmail.com>
2019-06-05 11:14:12 -0300

    escolas init comb

--- codes/combineDAta.py
+++ codes/combineDAta.py
@@ -48,27 +48,68 @@ df2 = df2.select('ID_ALUNO','IN_SITUACAO_CENSO', 'ID_ESCOLA', 'ID_TURMA', 'ID_LO
 # IMPORT AND CONCAT DATA
 #========================
 
-def csvCombiner(strdata, filename, sep):
+# define columns for selections
+col_turma = ['ID_TURMA','NO_TURMA','NU_ANO_CENSO','CO_MUNICIPIO','CO_UF','TP_DEPENDENCIA','NU_DURACAO_TURMA']
+
+col_escola = ['CO_ENTIDADE','NO_ENTIDADE','DT_ANO_LETIVO_INICIO','CO_MUNICIPIO','TP_LOCALIZACAO',
+              'TP_DEPENDENCIA','IN_LOCAL_FUNC_UNID_PRISIONAL','IN_LOCAL_FUNC_PRISIONAL_SOCIO','IN_AGUA_FILTRADA',
+              'IN_AGUA_INEXISTENTE','IN_ENERGIA_INEXISTENTE','IN_ESGOTO_INEXISTENTE','IN_LIXO_RECICLA',
+              'IN_LABORATORIO_CIENCIAS','IN_LABORATORIO_INFORMATICA','IN_QUADRA_ESPORTES','IN_BIBLIOTECA_SALA_LEITURA',
+              'IN_BANHEIRO_DENTRO_PREDIO','IN_BANHEIRO_CHUVEIRO','IN_AREA_VERDE','IN_COMPUTADOR','IN_INTERNET','IN_ALIMENTACAO'
+              #'QT_COMP_ALUNO','QT_FUNCIONARIOS','QT_SALAS_EXISTENTES', 'QT_SALAS_UTILIZADAS'
+              # 
+            ]
+
+## function
+def csvCombiner(strdata, filename, sep, cols):
     ''' Concatenate each dataset  with the same name
         located in folders and subfolders
     '''
     camino = 'data/' + strdata  + '/'
     paths = next(os.walk(camino))[1]
     key = True
-    for i in paths[:-1]:
-        listin = os.listdir((camino + i))
-        for file in listin:
-            if filename in file:
-                imp = camino + i + '/' + file
-                if key == True:
-                    df = spark.read.csv(imp, header=True, inferSchema=True, sep=sep)
-                    # df = df.select(cols)
-                    key = False
-                else:
-                    x = spark.read.csv(imp, header=True, inferSchema=True, sep=sep)
-                    #x = x.select(cols)
-                    df = df.union(x)
+    for i in paths:
+        if i == '2018' or i == '2017' or i == '2015': # AJEITAR MATCH DE COLUNAS
+            listin = os.listdir((camino + i))
+            for file in listin:
+                if filename in file:
+                    imp = camino + i + '/' + file
+                    if key == True:
+                        df = spark.read.csv(imp, header=True, inferSchema=True, sep=sep)
+                        df = df.select(cols)
+                        key = False
+                    else:
+                        x = spark.read.csv(imp, header=True, inferSchema=True, sep=sep)
+                        x = x.select(cols)
+                        df = df.union(x)
     return df
 
-turmaDATA = csvCombiner('censo-escolar', 'TURMA', '|')
+#---- combinar dados ESCOLA
+censo_esc = csvCombiner('censo-escolar', 'ESCOLA', '|', col_escola)
+
+#---- combinar dados TURMA
 alunoSAEB = csvCombiner('censo-escolar', 'TURMA', '|')
+
+#---- combinar dados SAEB
+
+
+'''
+x = spark.read.csv('data/censo-escolar/2018/ESCOLAS.CSV', header=True, inferSchema=True, sep='|')
+x = x.select(col_escola)
+
+y = spark.read.csv('data/censo-escolar/2017/ESCOLAS.CSV', header=True, inferSchema=True, sep='|')
+y = y.select(col_escola)
+
+w = spark.read.csv('data/censo-escolar/2015/ESCOLAS.CSV', header=True, inferSchema=True, sep='|')
+w = w.select(col_escola)
+
+a = x.union(y)
+a = a.union(w)
+
+2014 PARA BAIXO: VERIFICAR MATCH DE COLUNAS
+h = spark.read.csv('data/censo-escolar/2014/ESCOLAS.CSV', header=True, inferSchema=True, sep='|')
+h = h.select(col_escola)
+
+j = spark.read.csv('data/censo-escolar/2013/ESCOLAS.CSV', header=True, inferSchema=True, sep='|')
+j = j.select(col_escola)
+'''
