f11a137df1b065b36e28bd48ae801b4628c52bc6
claudiopacha <claudiomonteiro@protonmail.com>
2019-09-03 19:41:15 -0300

    update for diaguv

--- codes/big-data-processing.py
+++ codes/big-data-processing.py
@@ -23,26 +23,21 @@ Hierarquical Modelling
 
 # import modules
 import os
-from time import time
+import time 
 import pandas as pd
-import numpy as np
-import plotly.offline as py
-import cufflinks as cf
-
-# paths to spark and python3
-os.environ['PYSPARK_SUBMIT_ARGS'] = '--executor-memory 1G pyspark-shell'
-os.environ["SPARK_HOME"] = "/home/pacha/spark"
-os.environ["PYSPARK_PYTHON"]="/usr/bin/python3"
 
 # execute PYSPARK
 exec(open('/home/pacha/spark/python/pyspark/shell.py').read())
 # UI: 127.0.0.1:4040
 
-#==========================
+#===========================#
 # IMPORT AND CONCAT DATA
-#========================
+#==========================#
 
-# define columns for selections
+# import functions
+import pyspark.sql.functions as SF
+
+#---------- define columns for selections
 col_turma = ['ID_TURMA','NO_TURMA','NU_ANO_CENSO','CO_MUNICIPIO','CO_UF','TP_DEPENDENCIA','NU_DURACAO_TURMA']
 
 col_escola = ['CO_ENTIDADE','NO_ENTIDADE','DT_ANO_LETIVO_INICIO','CO_MUNICIPIO','TP_LOCALIZACAO',
@@ -50,88 +45,80 @@ col_escola = ['CO_ENTIDADE','NO_ENTIDADE','DT_ANO_LETIVO_INICIO','CO_MUNICIPIO',
               'IN_AGUA_INEXISTENTE','IN_ENERGIA_INEXISTENTE','IN_ESGOTO_INEXISTENTE','IN_LIXO_RECICLA',
               'IN_LABORATORIO_CIENCIAS','IN_LABORATORIO_INFORMATICA','IN_QUADRA_ESPORTES','IN_BIBLIOTECA_SALA_LEITURA',
               'IN_BANHEIRO_DENTRO_PREDIO','IN_BANHEIRO_CHUVEIRO','IN_AREA_VERDE','IN_COMPUTADOR','IN_INTERNET','IN_ALIMENTACAO'
-              #'QT_COMP_ALUNO','QT_FUNCIONARIOS','QT_SALAS_EXISTENTES', 'QT_SALAS_UTILIZADAS'
-              # 
+              #'QT_COMP_ALUNO','QT_FUNCIONARIOS','QT_SALAS_EXISTENTES', 'QT_SALAS_UTILIZADAS' 
             ]
 
-col_nota = 'ID_MUNICIPIO ID_AREA ID_ESCOLA ID_DEPENDENCIA_ADM ID_LOCALIZACAO ID_TURMA ID_TURNO ID_SERIE ID_ALUNO PROFICIENCIA_LP PROFICIENCIA_LP_SAEB PROFICIENCIA_MT PROFICIENCIA_MT_SAEB'.split(' ')
+col_nota = 'ID_MUNICIPIO ID_AREA ID_ESCOLA ID_DEPENDENCIA_ADM ID_LOCALIZACAO ID_TURMA ID_TURNO ID_SERIE ID_ALUNO PROFICIENCIA_MT'.split(' ')
 
-## function
-def csvCombiner(strdata, filename, sep, cols):
+
+##------------------ function for loading
+def csvCombiner(strdata, filename, sep, cols, anoinit, anoend):
     ''' Concatenate each dataset  with the same name
         located in folders and subfolders
     '''
+    # load SF
+    import pyspark.sql.functions as SF
+    # define variables
     camino = 'data/' + strdata  + '/'
     paths = next(os.walk(camino))[1]
     key = True
-    for i in paths:
-        if i == '2018' or i == '2017' or i == '2016' or i == '2015': # AJEITAR MATCH DE COLUNAS
-            listin = os.listdir((camino + i))
-            for file in listin:
-                if filename in file:
-                    imp = camino + i + '/' + file
-                    if key == True:
-                        df = spark.read.csv(imp, header=True, inferSchema=True, sep=sep)
-                        df = df.select(cols)
-                        key = False
-                    else:
-                        x = spark.read.csv(imp, header=True, inferSchema=True, sep=sep)
-                        x = x.select(cols)
-                        df = df.union(x)
+    anos = [str(x) for x in range(anoinit, anoend+1)]
+    # verify data
+    if strdata == 'censo-escolar':
+        column = 'CO_MUNICIPIO'
+    elif strdata == 'saeb':
+        column = 'ID_MUNICIPIO'
+    # loop import
+    for i in anos:
+        listin = os.listdir((camino + i))
+        for file in listin:
+            if filename in file:
+                print(file)
+                imp = camino + i + '/' + file
+                print(imp)
+                if key == True:
+                    df = spark.read.csv(imp, header=True, inferSchema=True, sep=sep)
+                    df = df.filter(df[column] == 2611606) ### SELECT RECIFE
+                    df = df.select(cols)
+                    df = df.withColumn('ano', SF.lit(i))
+                    key = False
+                else:
+                    x = spark.read.csv(imp, header=True, inferSchema=True, sep=sep)
+                    x = x.filter(x[column] == 2611606) ### SELECT RECIFE
+                    x = x.select(cols)
+                    x = x.withColumn('ano', SF.lit(i))
+                    df = df.union(x)
     return df
 
-#---- combinar dados ESCOLA
-censo_esc = csvCombiner('censo-escolar', 'ESCOLA', '|', col_escola)
+#----------- CENSO ESCOLAR [ESCOLAR] -------------#
 
-#---- combinar dados TURMA
-alunoSAEB = csvCombiner('censo-escolar', 'TURMA', '|', col_turma)
+# generate data
+start_time = time.time()
+censo_esc_rec = csvCombiner('censo-escolar', 'ESCOLA', '|', col_escola, 2015, 2018)
+print("--- %s seconds ---" % (time.time() - start_time))
 
-#---- combinar dados SAEB
+# save data
+pd1 = censo_esc_rec.toPandas()
+pd1.to_csv('results/data/censo_esc_rec.csv')
 
-#===========================#
-# Exploratory Analysis
-#==========================#
+#--------- CENSO ESCOLAR [TURMA] ----------#
 
-#============ CENSO-ESCOLAR ESCOLA ===========#
+# generate data
+censo_turma = csvCombiner('censo-escolar', 'TURMA', '|', col_turma, 2015, 2018)
 
-# load 2017 data for testing
-censo_esc = spark.read.csv('data/censo-escolar/2017/ESCOLAS.CSV', header=True, inferSchema=True, sep='|')
-censo_esc = censo_esc.select(col_escola)
+#------------- ANEB ---------------#
 
-# Group by city
-colsel = ['IN_AGUA_FILTRADA','IN_AGUA_INEXISTENTE',
-         'IN_ENERGIA_INEXISTENTE','IN_ESGOTO_INEXISTENTE',
-        'IN_LABORATORIO_CIENCIAS','IN_LABORATORIO_INFORMATICA',
-        'IN_QUADRA_ESPORTES','IN_BIBLIOTECA_SALA_LEITURA',
-        'IN_BANHEIRO_DENTRO_PREDIO','IN_BANHEIRO_CHUVEIRO',
-        'IN_INTERNET', 'CO_MUNICIPIO']
+# generate data and verify time of execution
+start_time = time.time()
+aneb_rec = csvCombiner('saeb', 'TS_ALUNO', ',', col_nota, 2013, 2017)
+print("--- %s seconds ---" % (time.time() - start_time))
 
-muni = censo_esc.select(colsel).groupby('CO_MUNICIPIO').sum()
-muni.show(10)
+# save data
+pd2 = aneb_rec.toPandas()
+pd2.to_csv('results/data/aneb_rec.csv')
 
-# renomear colunas
-newColumns = ['code_muni'] + [x.lower() for x in colsel]
-muni = muni.toDF(*newColumns)
-muni.show(10)
 
-# select info columns
-muni = censo_esc.select(colsel).groupby('CO_MUNICIPIO').sum()
-
-# convert data and save locally FALTA NOMES DOS COLUNAS, ANO E ETC.
-muni.toPandas().to_csv('muni.csv')
-
-# select data from city
-#city = censo_esc.filter(censo_esc.CO_MUNICIPIO == 3158953)
-
-#================ INEP NOTAS ===============#
-
-notas = spark.read.csv('data/saeb/2017/TS_ALUNO_3EM_AG.csv', header=True, inferSchema=True, sep=',')
-notas = notas.select(col_nota)
-
-notas.printSchema()
-censo_esc.printSchema()
-
-#============== Combine data ===============#
+#============== COMBINE DATA ===============#
 
 # contar numero de casos por base
 print((censo_esc.count(), len(censo_esc.columns)))
